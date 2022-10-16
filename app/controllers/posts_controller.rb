33491728b0b62a517823a82c400bac53afdddf31
Mauricio Lima <mauricio@collateralxp.com>
2022-10-16 14:14:11 -0300

    add create posts flow

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params.merge(user_id: current_user.id))
+
+    if @post.valid?
+      @post.save
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
