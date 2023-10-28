00a5dbd5ff39c5f0168a9b38e0915ac2ec303f1c
Mauricio Lima <mauricio@collateralxp.com>
2023-10-28 12:05:28 -0300

    finish comments controller spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,16 +1,47 @@
 describe CommentsController do
   let(:user) { create(:user) }
-  let(:post) { create(:post, user: user) }
+  let(:post_object) { create(:post, user: user) }
 
   before { sign_in user }
 
   describe 'GET #new' do
-    subject(:get_new) { get :new, params: { post_id: post.id }, as: :turbo_stream }
+    subject(:get_new) { get :new, format: :turbo_stream, params: { post_id: post_object.id } }
 
     it do
       get_new
 
       expect(response).to be_successful
+    end
+  end
+
+  describe 'POST #create' do
+    subject(:post_create) do
+      post :create, format: :turbo_stream,
+                    params: { post_id: post_object.id, comment: comment_params }
+    end
+
+    context 'when success' do
+      let(:comment_params) { { body: 'This is a valid comment body' } }
+
+      it { expect { post_create }.to change(Comment, :count).by(1) }
+
+      it do
+        post_create
+
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when failure' do
+      let(:comment_params) { { body: '' } }
+
+      it { expect { post_create }.not_to change(Comment, :count) }
+
+      it do
+        post_create
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 end
